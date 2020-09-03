52ad37ec26cf9a33cd104951f0a7505532dbae3d
Mikyla Moehlenbruck <m.moehlenbruck11@gmail.com>
2020-09-03 14:48:37 -0700

    removing copied files

--- tap.rb
+++
@@ -1,9 +0,0 @@
-(1..10).tap { |x| p x }
-(1..10).to_a.tap { |x| p x }
-(1..10).select {|x| x.even? }.tap { |x| p x }
-(1..10).map {|x| x*x }.tap { |x| p x }
-
-p (1..10)
-p (1..10).to_a
-p (1..10).select {|x| x.even? }
-p (1..10).map {|x| x*x }
